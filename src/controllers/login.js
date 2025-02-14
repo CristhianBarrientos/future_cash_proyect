2d34a3b2f81aee7c88d1135b7ea65d4a65c87c7a
CristhianBarrientos <cbarrientosm1@miumg.edu.gt>
2025-02-14 00:11:50 -0600

    feat: se trabajo en la logica de la pantalla del signup, se implemento codigo nuevo y se integran datos aun con correcciones pendientes en lel desarrollo del proyecto@

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const botonIngresar = document.getElementById('buttom-login');
-    const botonRegistrar = document.getElementById('buttom-signup');
 
-    
     document.getElementById("show-signup").addEventListener("click", function (e) {
         e.preventDefault();
+        clearElements();
         document.getElementById("login-form").classList.add("hidden");
         document.getElementById("signup-form").classList.add("show");
     });
-
+    // boton volver
     document.getElementById("show-login").addEventListener("click", function (e) {
         e.preventDefault();
+        clearElements(); // al regrsar al logiin se va a limpiar todo el formulario y se le va a quitar el formato
         document.getElementById("signup-form").classList.remove("show");
         document.getElementById("login-form").classList.remove("hidden");
     });
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             }
         }
-         else {
+        else {
             Swal.fire({
                 title: "Campos incompletos...",
                 text: "LLena los campos antes de presionar sobre el boton ingresar!!",
@@ -91,42 +91,124 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // botonRegistrar.addEventListener('click', async (e) => {
-    //     const frstnameusr = document.getElementById("frst-name-user").value;
-    //     const frstScndName = document.getElementById("frst-lstname-user").value;
-    //     const email = document.getElementById("email").value;
-    //     const usuario = document.getElementById("new-username").value;
-    //     const contrasena = document.getElementById("new-username","new-password").value;
-    //     const inputs = document.querySelectorAll('signup-form input');
-    //     const values = Array.from(inputs).map(input => input.value);
+    const mensajeEmail = document.getElementById("mensajeEmail");
+    document.getElementById('email').oninput = function () {
+        const email = document.getElementById('email');
+        const emailValor = email.value;
+        const emailPatron = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+        if (emailPatron.test(emailValor)) {
+            mensajeEmail.textContent = "";
+        } else {
+            mensajeEmail.textContent = "Ingresa un correo real"
+        }
+    };
 
-    //     if(values){
-    //         Swal.fire({
-    //             title: "Capos llenos!!!!",
-    //             text: "Texto de pruebas... ",
-    //             icon: "success",
-    //             // timer: 2000,
-    //             showConfirmButton: true,
-    //         });
-    //         console.log(values);
+    var requiredInputs = document.querySelectorAll('input[required]');
+
+    // evento que verifica cuando la persona esta escribiendo en el input, y se ejecuta 
+    requiredInputs.forEach(function (input) {
+        input.addEventListener("input", function () {
+            requiredFunction();
+        });
+    });
+
+    // funcion que realiza la verificacion de los campos obligatorios 
+    function requiredFunction() {
+        requiredInputs.forEach(function (input) {
+            const frstnameusr = document.getElementById("frst-name-user").value;
+            const scndnameuser = document.getElementById("scnd-name-user").value;
+            const frstLstName = document.getElementById("frst-lstname-user").value;
+            const scndLstName = document.getElementById("scnd-lstname-user").value;
+            const email = document.getElementById("email").value;
+            const usuario = document.getElementById("new-username").value;
+            const contrasena = document.getElementById("new-password").value;
+            try {
+                var varPrint = ""
+                if (frstnameusr === "" || scndnameuser === "" || frstLstName === "" || scndLstName === "" || email === "" || usuario === "" || contrasena === "") {
+                    // Implementacion de operadores ternarios para reducir el volumen del codigo (introduccion a lambdas en javascript) 
+                    // PRIMER NOMBRE
+                    varPrint = document.getElementById("frst-name-user")
+                    frstnameusr === "" ? styleErr(varPrint) : removeStyleErr(varPrint);
+                    // SEGUNDO NOMBRE
+                    varPrint = document.getElementById("scnd-name-user")
+                    scndnameuser ==="" ? styleErr(varPrint) : removeStyleErr(varPrint);
+                    // PRIMER APELLIDO  
+                    varPrint = document.getElementById("frst-lstname-user")
+                    frstLstName ==="" ? styleErr(varPrint) : removeStyleErr(varPrint);
+                    // SEGUNDO APELLIDO
+                    varPrint = document.getElementById("scnd-lstname-user")
+                    scndLstName === "" ? styleErr(varPrint) : removeStyleErr(varPrint);
+                    // CORREO ELECTRONICO
+                    varPrint = document.getElementById("email")
+                    email === "" ? styleErr(varPrint) : removeStyleErr(varPrint);
+                    // NOMBRE DE USUARIO
+                    varPrint = document.getElementById("new-username")
+                    usuario === "" ? styleErr(varPrint) : removeStyleErr(varPrint);
+                    // CONTRASENIA
+                    varPrint = document.getElementById("new-password")
+                    contrasena === "" ? styleErr(varPrint) : removeStyleErr(varPrint);
+                }
+
+            } catch (error) {
+                console.log(error)
+            }
+        });
+    }
+
+    function styleErr(varPrint) {
+        if (varPrint && varPrint.style) {
+            // varPrint.style.border = '1.5px dotted red';
+            varPrint.classList.add('input-error');
+        }
+    }
+    function removeStyleErr(varPrint) {
+        if (varPrint && varPrint.style) {
+            // varPrint.style.border = '';
+            varPrint.classList.remove('input-error');
+            varPrint.classList.add('right-side');
+        }
+    }
+
+    function clearElements() {
+        var inputs = document.querySelectorAll("input[required]");
+
+        inputs.forEach(function (input) {
+            input.value = "";
+            input.classList.remove('input-error');
+            mensajeEmail.textContent = ""
+        });
+
+        // inputs.forEach(function (input) {
+        //     input.classList.remove('input-error');
+        // });
+    }
+
+    // // campos incompletos 
+    //     Swal.fire({
+    //         title: "Campos incompletos!!!!",
+    //         text: "Asegurese de llenar todos los campos obligatorios antes de continuar... ",
+    //         icon: "warning",
+    //         // timer: 2000,
+    //         showConfirmButton: true,
+    //     });
+    //     // console.log(values);
+
+    // // campos completos 
+    // for (var i = 0; i < inputs.length; i++) {
+    //     if (inputs !== "") {
+    //         inputs[i].classList.remove('input-error');
+    //         inputs[i].classList.add('right-side');
     //     }
-    //     else{
-    //         Swal.fire({
-    //             title: "Campos incompletos!!!!",
-    //             text: "Texto de pruebas... ",
-    //             icon: "warning",
-    //             // timer: 2000,
-    //             showConfirmButton: true,
-    //         });
-    //         console.log(values);
-    //     }
+    // }
 
-
+    // Swal.fire({
+    //     title: "Capos llenos!!!!",
+    //     text: "Texto de pruebas... ",
+    //     icon: "success",
+    //     // timer: 2000,
+    //     showConfirmButton: true,
     // });
-    // setTimeout(
-    //     function() { 
-    //         document.querySelector('.slogan-style').style.color = 'black'; 
-    //     }, 1000);
-    
+    // // console.log(values);
+
 });
